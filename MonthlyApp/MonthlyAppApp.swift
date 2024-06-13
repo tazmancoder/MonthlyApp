96c942d628b080adff3dd3e0578e27ebb264e087
Mark J Perryman <tazmancoder@gmail.com>
2024-06-13 12:27:31 -0600

    Initial Commit

---
+++ MonthlyApp/MonthlyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyAppApp.swift
+//  MonthlyApp
+//
+//  Created by Mark Perryman on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
